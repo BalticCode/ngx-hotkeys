a6e4496d8a05009c75a1a8a89d1552254a95af39
Basti Hoffmann <bo.hoffi@gmail.com>
2019-08-12 23:23:51 +0200

    removed directive

--- projects/ngx-hotkeys/src/public_api.ts
+++ projects/ngx-hotkeys/src/public_api.ts
@@ -2,6 +2,5 @@
  * Public API Surface of ngx-hotkeys
  */
 export * from './lib/ngx-hotkeys.service';
-export * from './lib/ngx-hotkeys.directive';
 export * from './lib/ngx-cheatsheet/ngx-cheatsheet.component';
 export * from './lib/ngx-hotkeys.module';
