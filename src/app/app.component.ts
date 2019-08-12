4190e72cb2f05ae316796165602794d2bec227d0
Basti Hoffmann <bo.hoffi@gmail.com>
2019-08-12 23:25:05 +0200

    updated example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+
 import {NgxHotkeysService} from '@balticcode/ngx-hotkeys';
 
 @Component({
@@ -10,10 +11,9 @@ export class AppComponent {
 
   constructor(private _hotkeysService: NgxHotkeysService) {
     this._hotkeysService.register({
-      combo: 'shift.g',
+      combo: 'shift+g',
       handler: () => {
         console.log('Typed hotkey');
-        return false; // Prevent bubbling
       },
       description: 'Send a secret message to the console.'
     });
