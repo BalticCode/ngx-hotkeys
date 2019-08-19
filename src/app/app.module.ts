bc4851cb64a95a090948d12b0dfbb25514f0e084
Basti Hoffmann <bo.hoffi@gmail.com>
2019-08-19 17:19:45 +0200

    updated example module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { NgxHotkeysModule } from '@balticcode/ngx-hotkeys';
 import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxHotkeysModule } from '@balticcode/ngx-hotkeys';
 
 @NgModule({
   declarations: [
@@ -17,12 +17,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatToolbarModule,
     MatCardModule,
     MatListModule,
-
-    NgxHotkeysModule.forRoot({
-      cheatSheetCloseEsc: true
-    }),
-
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    NgxHotkeysModule.forRoot()
   ],
   bootstrap: [AppComponent]
 })
