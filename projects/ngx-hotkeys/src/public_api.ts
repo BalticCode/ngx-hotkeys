306824480d1a2cfb1c5a76fdbd6156fa23d746df
Basti Hoffmann <bo.hoffi@gmail.com>
2020-02-29 23:20:47 +0100

    expose Hotkey and configuration interface

--- projects/ngx-hotkeys/src/public_api.ts
+++ projects/ngx-hotkeys/src/public_api.ts
@@ -1,6 +1,7 @@
 /*
  * Public API Surface of ngx-hotkeys
  */
+export { Hotkey, HotkeyOptions } from './lib/interfaces';
 export * from './lib/services/ngx-hotkeys.service';
 export * from './lib/ngx-cheatsheet/ngx-cheatsheet.component';
 export * from './lib/ngx-hotkeys.module';
